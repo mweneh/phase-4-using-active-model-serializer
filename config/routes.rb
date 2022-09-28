620b19244a8aeee30c8b3431fdeee6fc0717a6cf
David <davidongaroh@gmail.com>
2022-09-28 21:12:57 +0300

    solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
+
+  get '/movies/:id/summary', to: 'movie_summaries#summary'
+  get '/movie_summaries' => 'movie_summaries#summaries'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
